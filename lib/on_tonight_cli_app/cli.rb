3fd297d488fc64421f60cee01fd307ebc9ffbe12
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-09-27 20:50:11 +0000

    experimenting with UI, added stubs for show and episode descriptions

--- lib/on_tonight_cli_app/cli.rb
+++ lib/on_tonight_cli_app/cli.rb
@@ -1,14 +1,33 @@
 class OnTonight::CLI
 
   def call
+    puts "So, what's on tonight?"
     list_shows
     menu
   end
 
   def list_shows
-    puts "So, what's on tonight?"
     puts "1. The Blacklist | 8:00 PM | NBC"
     puts "2. Modern Family | 9:00 PM | ABC"
+  end
+
+  def episode
+    puts "The Blacklist | 8:00 PM | NBC"
+    puts "------------------------------"
+    puts "Smokey Putnum (Season 5 | Episode 1)"
+    puts "************************************"
+    puts "In the Season 5 premiere, Red — whose criminal empire is in ruins—enlists Liz in an unlikely plan to simultaneously earn cash and deliver a new Blacklister to the Task Force. As Liz faces the new reality of working alongside her father, Ressler finds himself in a precarious position and Tom weighs his options."
+  end
+
+  def show
+    puts "The Blacklist | NBC | Metascore: 74"
+    puts "------------------------------"
+    puts "A most-wanted fugitive works with a rookie FBI profiler to take down criminals and terrorists in this crime series."
+    puts "************************************"
+    puts "Starring:"
+    puts "James Spader as Raymond Reddington"
+    puts "Ryan Eggold as Tom Keen"
+    puts "Mozhan Marno as Samar Novabi"
   end
 
   def menu
@@ -16,6 +35,7 @@ class OnTonight::CLI
     while input != "exit"
       puts "Which show would you like more info on? (Enter a number)"
       input = gets.strip.downcase
+
       case input
       when "1"
         puts "The Blacklist | 8:00 PM | NBC"
@@ -23,6 +43,20 @@ class OnTonight::CLI
         puts "Would you like to see more info about"
         puts "1. Tonight's episode"
         puts "2. The show"
+        input = gets.strip.downcase
+          case input
+          when "1"
+            puts "#{episode}"
+            puts "What would you like to do now?"
+            puts "1. Show Description"
+          when "2"
+            puts "What would you like to do now?"
+            puts "1. Episode Decription"
+            puts "Show Description"
+          end
+          puts "2. List shows"
+          input = gets.strip.downcase
+
       end
     end
   end
