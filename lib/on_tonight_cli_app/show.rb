cb8e128e188e8c6ff41361e699e758e61ec682cb
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-10-01 19:49:57 +0000

    initialized Show class

--- lib/on_tonight_cli_app/show.rb
+++ lib/on_tonight_cli_app/show.rb
@@ -1,3 +1,7 @@
 class OnTonight::Show
-  attr_accessor :name, :network, :score, :description, :episode
+  attr_accessor :name, :network, :description, :episode
+
+  def initialize(name=nil,network=nil,description=nil,episode=nil)
+
+  end
 end
