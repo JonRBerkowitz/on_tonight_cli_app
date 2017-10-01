d6a3cf247103b5bb3a7df703526b079c18093afd
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-10-01 19:42:12 +0000

    Began writing scraper class

--- lib/on_tonight_cli_app/scraper.rb
+++ lib/on_tonight_cli_app/scraper.rb
@@ -1,3 +1,11 @@
 class OnTonight::Scraper
 
+  def get_page
+    Nokogiri::HTML(open("http://www.tvguide.com/new-tonight"))
+  end
+
+  def scrape_show_index
+    puts self.get_page.css(".listings-program-content")
+  end
+
 end
