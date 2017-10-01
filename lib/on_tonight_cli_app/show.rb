90bd9abb2a947c798b32993d7dcf5c1c53bdbecb
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-10-01 20:51:00 +0000

    starting to pick out correct finders for each piece of info

--- lib/on_tonight_cli_app/show.rb
+++ lib/on_tonight_cli_app/show.rb
@@ -4,12 +4,19 @@ class OnTonight::Show
   @@all = []
 
   def initialize(name=nil, network=nil, description=nil, episode=nil)
-
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def self.new_show(i)
+    show = self.new
+    show.name = i.css(".listings-program-title").text
+    puts "#{show.name}"
+    show.episode = i.css(".listings-program-link").text
+    puts "#{show.episode}"
+  end
 
 end
