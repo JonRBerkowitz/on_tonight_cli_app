93dd806dd30cda15903eba2ba5ff6b6c682eef4d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-10-02 02:24:33 +0000

    added scraping for individual show page urls

--- lib/on_tonight_cli_app/show.rb
+++ lib/on_tonight_cli_app/show.rb
@@ -3,7 +3,7 @@ class OnTonight::Show
 
   @@all = []
 
-  def initialize(name=nil, network=nil, description=nil, episode=nil)
+  def initialize(name=nil, network=nil, description=nil, episode_title=nil, episode_description=nil, time=nil)
     @@all << self
   end
 
@@ -14,15 +14,12 @@ class OnTonight::Show
   def self.new_show(i)
     show = self.new
     show.name = i.css(".listings-program-title").text
-    puts "#{show.name}"
     show.time = i.css(".listings-program-airing-info").text.split(" | ")[0]
-    puts "#{show.time}"
     show.network = i.css(".listings-program-airing-info").text.split(" | ")[1]
-    puts "#{show.network}"
     show.episode_title = i.css(".listings-program-episode-title .listings-program-link").text
-    puts "#{show.episode_title}"
     show.episode_description = i.css(".listings-program-description").text
-    puts "#{show.episode_description}"
+    show.url = i.css("a.listings-program-link").attribute("href").value
+    show
   end
 
 end
